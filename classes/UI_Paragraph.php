36160cf6d99b11dfdea51597b2a9ade70bc4693d
delight.im <marco1@live.de>
2014-02-07 07:11:28 +0100

    Added support for preformatted text in pagraphs

--- classes/UI_Paragraph.php
+++ classes/UI_Paragraph.php
@@ -6,8 +6,9 @@ class UI_Paragraph extends UI {
 
     protected $uiInstance;
     protected $text;
+    protected $isPreformatted;
 
-    public function __construct($textOrUI) {
+    public function __construct($textOrUI, $isPreformatted = false) {
         if ($textOrUI instanceof UI) {
             $this->uiInstance = $textOrUI;
         }
@@ -17,18 +18,23 @@ class UI_Paragraph extends UI {
         else {
             throw new Exception('Paragraph content must be an instance of class UI or a text string');
         }
+        $this->isPreformatted = $isPreformatted;
     }
 
     public function getHTML() {
-        $out = '<p>';
         if (isset($this->uiInstance)) {
-            $out .= $this->uiInstance->getHTML();
+            $out = $this->uiInstance->getHTML();
         }
         else {
-            $out .= $this->text;
+            $out = $this->text;
         }
-        $out .= '</p>';
-        return $out;
+
+        if ($this->isPreformatted) {
+            return '<pre class="pre-scrollable">'.htmlspecialchars($out).'</pre>';
+        }
+        else {
+            return '<p>'.$out.'</p>';
+        }
     }
 
 }
