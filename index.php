a60b2c03ddb6d79603bd106c1be544ae637fc3f1
Marco <marco1@live.de>
2014-04-22 05:09:28 +0200

    Added throttling for sign up (account spamming)
    Thanks, faisalahmed @ HackerOne!

--- index.php
+++ index.php
@@ -27,40 +27,47 @@ if (isset($_GET['v'])) {
 elseif (UI::isPage('sign_up')) {
     if (UI::isAction('sign_up')) {
         if (UI_Form::isCSRFTokenValid($_POST)) {
-            $data = UI::getDataPOST('sign_up');
+            if (Database::Throttling_isAllowed(UI::getIPAddress(), 'sign_up')) {
+                $data = UI::getDataPOST('sign_up');
 
-            $data_type = isset($data['type']) ? intval($data['type']) : 0;
-            if (!Authentication::isAllowSignUpDevelopers()) {
-                $data_type = User::TYPE_TRANSLATOR;
-            }
-            $data_username = isset($data['username']) && is_string($data['username']) ? trim($data['username']) : '';
-            $data_password1 = isset($data['password1']) && is_string($data['password1']) ? trim($data['password1']) : '';
-            $data_password2 = isset($data['password2']) && is_string($data['password2']) ? trim($data['password2']) : '';
+                $data_type = isset($data['type']) ? intval($data['type']) : 0;
+                if (!Authentication::isAllowSignUpDevelopers()) {
+                    $data_type = User::TYPE_TRANSLATOR;
+                }
+                $data_username = isset($data['username']) && is_string($data['username']) ? trim($data['username']) : '';
+                $data_password1 = isset($data['password1']) && is_string($data['password1']) ? trim($data['password1']) : '';
+                $data_password2 = isset($data['password2']) && is_string($data['password2']) ? trim($data['password2']) : '';
 
-            if (Authentication::isPasswordAllowed($data_password1, $data_password2)) {
-                if (mb_strlen($data_username) >= 3) {
-                    if ($data_type == User::TYPE_TRANSLATOR || $data_type == User::TYPE_DEVELOPER) {
-                        $data_password = password_hash($data_password1, PASSWORD_BCRYPT);
-                        try {
-                            Database::insert("INSERT INTO users (username, password, type, join_date) VALUES (".Database::escape($data_username).", ".Database::escape($data_password).", ".intval($data_type).", ".time().")");
-                            // show password as a hidden value masked by asterisks which can be clicked to expose the password
-                            $passwordShowable = '<code onclick="this.innerHTML = \''.UI::htmlspecialcharsJS($data_password1).'\';" style="cursor:pointer;">'.str_repeat('*', mb_strlen($data_password1)).'</code>';
-                            $alert = new UI_Alert('<p>Your free account has been created!</p><p>Please sign in by entering your username and password in the top-right corner.</p><p>Username: <code>'.htmlspecialchars($data_username).'</code></p><p>Password: '.$passwordShowable.' (click to show)</p><p>Please remember your password as we won\'t be able to recover access to your account in case you forget it.</p>', UI_Alert::TYPE_SUCCESS);
+                if (Authentication::isPasswordAllowed($data_password1, $data_password2)) {
+                    if (mb_strlen($data_username) >= 3) {
+                        if ($data_type == User::TYPE_TRANSLATOR || $data_type == User::TYPE_DEVELOPER) {
+                            $data_password = password_hash($data_password1, PASSWORD_BCRYPT);
+                            try {
+                                Database::insert("INSERT INTO users (username, password, type, join_date) VALUES (".Database::escape($data_username).", ".Database::escape($data_password).", ".intval($data_type).", ".time().")");
+                                Database::Throttling_increaseCounter(UI::getIPAddress(), 'sign_up');
+
+                                // show password as a hidden value masked by asterisks which can be clicked to expose the password
+                                $passwordShowable = '<code onclick="this.innerHTML = \''.UI::htmlspecialcharsJS($data_password1).'\';" style="cursor:pointer;">'.str_repeat('*', mb_strlen($data_password1)).'</code>';
+                                $alert = new UI_Alert('<p>Your free account has been created!</p><p>Please sign in by entering your username and password in the top-right corner.</p><p>Username: <code>'.htmlspecialchars($data_username).'</code></p><p>Password: '.$passwordShowable.' (click to show)</p><p>Please remember your password as we won\'t be able to recover access to your account in case you forget it.</p>', UI_Alert::TYPE_SUCCESS);
+                            }
+                            catch (Exception $e) {
+                                $alert = new UI_Alert('<p>It seems this username is already taken. Please try another one.</p>', UI_Alert::TYPE_WARNING);
+                            }
                         }
-                        catch (Exception $e) {
-                            $alert = new UI_Alert('<p>It seems this username is already taken. Please try another one.</p>', UI_Alert::TYPE_WARNING);
+                        else {
+                            $alert = new UI_Alert('<p>Please choose one of the account types!</p>', UI_Alert::TYPE_WARNING);
                         }
                     }
                     else {
-                        $alert = new UI_Alert('<p>Please choose one of the account types!</p>', UI_Alert::TYPE_WARNING);
+                        $alert = new UI_Alert('<p>Your username must be at least 3 characters long!</p>', UI_Alert::TYPE_WARNING);
                     }
                 }
                 else {
-                    $alert = new UI_Alert('<p>Your username must be at least 3 characters long!</p>', UI_Alert::TYPE_WARNING);
+                    $alert = new UI_Alert('<p>Please check the requirements for your password and make sure to re-type it correctly!</p>', UI_Alert::TYPE_WARNING);
                 }
             }
             else {
-                $alert = new UI_Alert('<p>Please check the requirements for your password and make sure to re-type it correctly!</p>', UI_Alert::TYPE_WARNING);
+                $alert = UI_Alert::forActionThrottled();
             }
         }
         else {
