b51882f13e931633d74e3bbd08156f9bf5980894
Marco <marco1@live.de>
2014-08-26 23:59:02 +0200

    Show phrase key on review screen

--- classes/UI.php
+++ classes/UI.php
@@ -764,10 +764,13 @@ abstract class UI {
                         $originalWithMarkedEntities = Phrase::markEntities($originalWithMarkedEntities, Phrase_Android::getPlaceholders($valueReference), 'text-primary', true);
                         $originalWithMarkedEntities = Phrase::markEntities($originalWithMarkedEntities, Phrase_Android::getHTMLTags($valueReference), 'text-success', true);
 
-                        $table->addRow(array('<strong>'.Language::getLanguageNameFull($repositoryData['defaultLanguage']).'</strong>', '<span dir="'.(Language::isLanguageRTL($repositoryData['defaultLanguage']) ? 'rtl' : 'ltr').'">'.nl2br($phraseWithMarkedEntities).'</span>'));
-                        $table->addRow(array('<strong>Old value</strong>', '<span dir="'.(Language::isLanguageRTL($languageID) ? 'rtl' : 'ltr').'">'.nl2br($originalWithMarkedEntities).'</span>'));
+                        $fullyQualifiedName = Phrase_Android::getFullyQualifiedName($referencedPhrase, $editData[0]['phraseSubKey']);
+                        $fullyQualifiedNameHTML = '<span dir="ltr" class="small" style="display:block; color:#999; margin-top:4px;">'.htmlspecialchars($fullyQualifiedName).'</span>';
+
+                        $table->addRow(array('<strong>'.Language::getLanguageNameFull($repositoryData['defaultLanguage']).'</strong>'.$fullyQualifiedNameHTML, '<span dir="'.(Language::isLanguageRTL($repositoryData['defaultLanguage']) ? 'rtl' : 'ltr').'">'.nl2br($phraseWithMarkedEntities).'</span>'));
+                        $table->addRow(array('<strong>Old value</strong>'.$fullyQualifiedNameHTML, '<span dir="'.(Language::isLanguageRTL($languageID) ? 'rtl' : 'ltr').'">'.nl2br($originalWithMarkedEntities).'</span>'));
                         $table->addRow(array('<strong>Applied changes</strong>', '<span dir="'.(Language::isLanguageRTL($languageID) ? 'rtl' : 'ltr').'">'.nl2br(htmlDiff(htmlspecialchars($valuePrevious), htmlspecialchars($editData[0]['suggestedValue']))).'</span>'));
-                        $table->addRow(array('<strong>New value</strong>', $newValueHTML));
+                        $table->addRow(array('<strong>New value</strong>'.$fullyQualifiedNameHTML, $newValueHTML));
                         $table->addRow(array('<strong>Submit time</strong>', date('d.m.Y H:i', $editData[0]['submit_time'])));
                         if ($isAllowedToReview) {
                             $table->addRow(array('<strong>Contributor</strong>', $contributorName));
