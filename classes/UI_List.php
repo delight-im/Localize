03528b907963daac7bc0d8330901e5246b7aa348
Marco <marco1@live.de>
2015-09-09 06:27:56 +0200

    Allow for lists to be either ordered or unordered

--- classes/UI_List.php
+++ classes/UI_List.php
@@ -5,17 +5,28 @@ require_once('UI.php');
 class UI_List extends UI {
 
     protected $items;
+    protected $ordered;
 
     public function __construct() {
         $this->items = array();
+        $this->ordered = false;
     }
 
     public function addItem($textOrUI) {
         $this->items[] = $textOrUI;
     }
 
+    public function setOrdered($ordered) {
+        $this->ordered = $ordered;
+    }
+
     public function getHTML() {
-        $out = '<ul>';
+        if ($this->ordered) {
+            $out = '<ol>';
+        }
+        else {
+            $out = '<ul>';
+        }
         foreach ($this->items as $item) {
             $out .= '<li>';
             if ($item instanceof UI) {
@@ -29,10 +40,13 @@ class UI_List extends UI {
             }
             $out .= '</li>';
         }
-        $out .= '</ul>';
+        if ($this->ordered) {
+            $out .= '</ol>';
+        }
+        else {
+            $out .= '</ul>';
+        }
         return $out;
     }
 
 }
-
-?>
